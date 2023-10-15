e6370e289d14015cc064478138f854706ddc0061
Saikrishna Reddy <117559736+saikrishna488@users.noreply.github.com>
2023-10-15 19:33:32 +0530

    fixed icon menu

--- backend/templates/portfolio1.js
+++ backend/templates/portfolio1.js
@@ -88,7 +88,7 @@ let str = `
         <a class="navbar-brand" href="#">${data.name}</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+            <span class="bi bi-list"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
